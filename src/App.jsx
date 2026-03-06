de4852b006a69e36b20782e828aee5f97abe53cd
Pratish <bhonglepratish@gmail.com>
2026-03-06 20:13:33 +0530

    Initial commit -GSAP learning

---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import IPhoneHero from './components/IPhoneHero'
+import MacLineup from './components/MacLineup'
+import IPhone from './components/iphone'
+import IPhoneCards from './components/IphoneCard'
+import AirPodsUnboxing from './components/AirPodsUnboxing'
+import WatchSection from './components/Watch'
+import IPadSection from './components/ipad'
+import Footer from './components/Footer'
+import Cart from './pages/Cart'
+
+const App = () => {
+  const [cart, setCart] = useState([]);
+
+  // Total quantity count for Navbar
+  const totalItems = cart.reduce((acc, item) => acc + item.quantity, 0);
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={
+          <>
+            <Navbar cartCount={totalItems} /> 
+            
+            <div id="hero"><Hero /></div>
+            
+            <div id="iphone-hero">
+              <IPhoneHero cart={cart} setCart={setCart} />
+            </div>
+
+            <div id="mac">
+              <MacLineup cart={cart} setCart={setCart} />
+            </div>
+
+            <div id="iphone">
+              <IPhone cart={cart} setCart={setCart} />
+              <IPhoneCards cart={cart} setCart={setCart} /> 
+            </div>
+
+            {/* FIX: In sab mein props pass karna zaroori hai */}
+            <div id="airpods">
+              <AirPodsUnboxing cart={cart} setCart={setCart} />
+            </div>
+
+            <div id="watch">
+              <WatchSection cart={cart} setCart={setCart} />
+            </div>
+
+            <div id="ipad">
+              <IPadSection cart={cart} setCart={setCart} />
+            </div>
+            
+            <Footer />
+          </>
+        } />
+        
+        <Route path="/cart" element={<Cart cart={cart} setCart={setCart} />} />
+        
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
